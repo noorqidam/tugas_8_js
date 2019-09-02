da065b730082afcac062bddfa6c897604f1a96f8
noorqidam <noorqidam@gmail.com>
2019-09-02 15:18:29 +0700

    Tugas 8 JS
    Tugas ini saya kumpulkan untuk Niomic

---
+++ tugas8.js
@@ -0,0 +1,11 @@
+function tugas8() {
+ var identitas = {
+   nama: 'Noor Qidam Mujahidin',
+   umur: 24,
+   tinggibadan: 170,
+   beratbadan: 65,
+ }
+ identitas.umur = 25;
+ console.log(identitas);
+}
+tugas8()
